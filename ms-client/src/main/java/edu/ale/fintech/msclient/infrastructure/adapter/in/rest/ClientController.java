3aa7b9fa3e06259af1e43c26c1d5170b0aec1049
R. Alejandro Fuentes <fuentesra@hotmail.com>
2026-01-09 10:42:36 -0300

    feat(ms-client): implement hexagonal architecture adn webclient communication

--- ms-client/src/main/java/edu/ale/fintech/msclient/infrastructure/adapter/in/rest/ClientController.java
+++ ms-client/src/main/java/edu/ale/fintech/msclient/infrastructure/adapter/in/rest/ClientController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/clients")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ClientController {
 
     private final GetClientUseCase getClientUseCase;
